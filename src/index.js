48363f7181bf33a6da3e84bf8ceed5eeec197677
Bohdan <night.prowler112@gmail.com>
2023-01-04 12:57:55 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { LanguageProvider } from "./context/language-context";
 import "./index.css";
 
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <LanguageProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </React.StrictMode>
 );
